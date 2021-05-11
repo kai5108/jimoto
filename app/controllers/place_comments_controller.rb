fd6f504848cdcdff697a91972c6ceca034a353bc
kai5108 <k.510.ex.24@gmail.com>
2021-05-11 12:44:43 +0000

    create

--- app/controllers/place_comments_controller.rb
+++ app/controllers/place_comments_controller.rb
@@ -1,6 +1,16 @@
 class PlaceCommentsController < ApplicationController
   def create
+    place = Place.find(params[:place_id])
+    comment = current_user.place_comments.new(place_commnet_params)
+    comment.place_id = place.id
+    comment.save
+    redirect_to place_path(place)
   end
   def destroy
   end
+  
+  private
+  def place_comment_params
+    params.require(:place_comment).permit(comment)
+    
 end
