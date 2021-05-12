2635a8375c5d337c41cfb623f6ff712817462256
kai5108 <k.510.ex.24@gmail.com>
2021-05-12 07:02:58 +0000

    ok

--- app/controllers/place_comments_controller.rb
+++ app/controllers/place_comments_controller.rb
@@ -1,16 +1,22 @@
 class PlaceCommentsController < ApplicationController
   def create
-    place = Place.find(params[:place_id])
-    comment = current_user.place_comments.new(place_commnet_params)
-    comment.place_id = place.id
-    comment.save
-    redirect_to place_path(place)
+    @place = Place.find(params[:place_id])
+    @comment = current_user.place_comments.build(place_comment_params)
+    @comment.place_id = @place.id
+    @comment.save
+      redirect_to request.referer
+    @place_comment = PlaceComment.new
   end
+  
   def destroy
+    @place = Place.find(params[:place_id])
+    @place_comment = PlaceComment.find(params[:id]).destroy
+    @place_comment.destroy
+      redirect_to request.referer
   end
   
   private
   def place_comment_params
-    params.require(:place_comment).permit(comment)
-    
+    params.require(:place_comment).permit(:comment)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :places, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
-    resource :place_comment, only: [:create, :destroy]
+    resources :place_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
 end
