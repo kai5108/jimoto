f144fc0ab131c829f9daee9df87ee577ed000aea
kai5108 <k.510.ex.24@gmail.com>
2021-05-16 07:34:33 +0000

    comment complete

--- app/controllers/place_comments_controller.rb
+++ app/controllers/place_comments_controller.rb
@@ -1,21 +1,22 @@
 class PlaceCommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
     @place = Place.find(params[:place_id])
     @comment = current_user.place_comments.build(place_comment_params)
     @comment.place_id = @place.id
     @comment.save
-      redirect_to request.referer
+      # redirect_to request.referer
     @place_comment = PlaceComment.new
     # kaminari
-    # @place_comments = @place.place_comments.page(params[:page]).per(10)
-    #   render :create
+    @place_comments = @place.place_comments.page(params[:page]).per(10)
+      # render :create
   end
 
   def destroy
     @place = Place.find(params[:place_id])
     @place_comment = PlaceComment.find(params[:id]).destroy
     @place_comment.destroy
-      redirect_to request.referer
+      # redirect_to request.referer
     # kaminari
     @place_comments = @place.place_comments.page(params[:page]).per(10)
   end
