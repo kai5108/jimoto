16a9886dadcac2b13ae23a82d13fea2048bc8956
kai5108 <k.510.ex.24@gmail.com>
2021-05-28 04:56:33 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get 'search/search'
   devise_for :users
   resources :places, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
     resources :place_comments, only: [:create, :destroy]
